362d2abda1031fc0ee712ef444ef15df40e0ffab
KulinShah <shahkulin98@gmail.com>
2017-03-21 17:47:36 +0530

    One way done

--- client/client.py
+++ client/client.py
@@ -1,12 +1,38 @@
-import os
-import socket
-"""
+import os, socket, hashlib
+
 def md5(fn):
     hash = hashlib.md5()
     with open(fn, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash.update(chunk)
-    return hash.hexdigest()"""
+    return hash.hexdigest()
+
+def downloadTCPfile(split_command):
+    with open(split_command[2], "wb") as f:
+        print "File Opened"
+        data = s.recv(1024)
+        s.send("Done")
+        while data!="END":
+            f.write(data)
+            #print data
+            data = s.recv(1024)
+            s.send("Done")
+
+def downloadUDPfile(split_command):
+    f_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    f_socket.sendto("abcd", ("", 51216))
+    with open(split_command[2], 'wb') as f:
+        print("File opened")
+        while True:
+            print("Receiving Data")
+            info, udpaddr = f_socket.recvfrom(1024)
+            print(info)
+            #print("Data=%s", (info))
+            if info=="END":
+                break
+            f.write(info)
+    f_socket.close()
+
 
 s = socket.socket()
 host = ""
@@ -16,38 +42,44 @@ s.connect((host, port))
 s.send("Hello server!")
 
 while True:
-    print "Prompt ->"
+    print "Prompt ->",
     command = raw_input()
     s.send(command)
     count=0
     split_command = command.split()
     if split_command[0]=="download":
         if split_command[1]=="TCP":
-            with open(split_command[2], "wb") as f:
-                print "File Opened"
-                data = s.recv(1024)
-                s.send("Done")
-                while data!="END":
-                    f.write(data)
-                    print data
-                    data = s.recv(1024)
-                    s.send("Done")
+            hashfs = s.recv(1024)
+            if os.path.isfile(split_command[2]):
+                hashfc = md5(split_command[2])
+                print str(hashfs), str(hashfc)
+                if str(hashfs)!=str(hashfc):
+                    s.send("yes")
+                    print "Downloading File"
+                    downloadTCPfile(split_command)
+                else:
+                    s.send("no")
+            else:
+                s.send("yes")
+                print "Downloading File"
+                downloadTCPfile(split_command)
         elif split_command[1]=="UDP":
-            f_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-            s.send("Done")
-            fileinfo = s.recv(1024)
-            f_socket.sendto("abcd", ("", 51216))
-            with open(split_command[2], 'wb') as f:
-                print("File opened")
-                while True:
-                    print("Receiving Data")
-                    info, udpaddr = f_socket.recvfrom(1024)
-                    print(info)
-                    #print("Data=%s", (info))
-                    if info=="END":
-                        break
-                    f.write(info)
-            f_socket.close()
+            hashfs = s.recv(1024)
+            if os.path.isfile(split_command[2]):
+                hashfc = md5(split_command[2])
+                if str(hashfs)!=str(hashfc):
+                    s.send("yes")
+                    print "Downloading File"
+                    downloadUDPfile(split_command)
+                else:
+                    s.send("no")
+            else:
+                s.send("yes")
+                print "Downloading File"
+                downloadUDPfile(split_command)
+    elif split_command[0]=="close":
+        print "Closing connection"
+        break
 
     data = s.recv(1024)
     s.send("done")
@@ -58,7 +90,5 @@ while True:
     print command
 #f.close()
 
-print('Successfully get the file')
-conn.close()
-ans = os.system('ls -l')
-print('connection closed')
+s.close()
+print('Connection closed')
--- server/server.py
+++ server/server.py
@@ -8,6 +8,131 @@ def md5(fn):
             hash.update(chunk)
     return hash.hexdigest()
 
+def indexLongList(split_command):
+    list_files = os.listdir('.')
+    #s.send('hi')
+    for fl in list_files:
+        info=""
+        fl_stat = os.stat(fl)
+        sz = str(fl_stat.st_size)
+        ct = time.ctime(fl_stat.st_ctime)
+        mt = time.ctime(fl_stat.st_mtime)
+        f_type, code = mimetypes.guess_type(fl, False)
+        if not f_type:
+            f_type = "Unknown"
+        info+='\t'.join([fl, sz, ct, mt, f_type])+'\n'
+        print info, len(info)
+        conn.send(info)
+        ans = conn.recv(1024)
+        print ans
+    conn.send("123")
+
+def indexShortList(split_command):
+    list_files = os.listdir('.')
+    for fl in list_files:
+        mint=time.mktime(time.strptime(' '.join(split_command[2:6]), '%d %m %Y %H:%M:%S'))
+        maxt=time.mktime(time.strptime(' '.join(split_command[6:10]), '%d %m %Y %H:%M:%S'))
+        info=""
+        fl_stat = os.stat(fl)
+        sz = str(fl_stat.st_size)
+        ct = time.ctime(fl_stat.st_ctime)
+        mt = time.ctime(fl_stat.st_mtime)
+        f_type, code = mimetypes.guess_type(fl, False)
+        if not f_type:
+            f_type = "Unknown"
+        print mt, mint, maxt
+        if mint <= os.path.getmtime(fl) <=maxt:
+            info+='\t'.join([fl, sz, ct, mt, f_type])+'\n'
+            print info, len(info)
+            conn.send(info)
+            ans = conn.recv(1024)
+            print ans
+    conn.send("123")
+
+def indexRegex(split_command):
+    list_files = os.listdir('.')
+    #s.send('hi')
+    reprog = re.compile(' '.join(split_command[2:]).strip())
+    for fl in list_files:
+        info=""
+        fl_stat = os.stat(fl)
+        sz = str(fl_stat.st_size)
+        ct = time.ctime(fl_stat.st_ctime)
+        mt = time.ctime(fl_stat.st_mtime)
+        f_type, code = mimetypes.guess_type(fl, False)
+        if not f_type:
+            f_type = "Unknown"
+        if reprog.match(fl):
+            print fl
+            info+='\t'.join([fl, sz, ct, mt, f_type])+'\n'
+            print info, len(info)
+            conn.send(info)
+            ans = conn.recv(1024)
+            print ans
+    conn.send("123")
+
+def hashVerify(split_command):
+    filename = split_command[2]
+    check_sum = md5(filename)
+    fl_stat = os.stat(filename)
+    mt = time.ctime(fl_stat.st_mtime)
+    st = str(check_sum) + " " + str(mt)
+    conn.send(st)
+    ans = conn.recv(1024)
+
+def hashCheckAll(split_command):
+    list_files = os.listdir('.')
+    for fl in list_files:
+        check_sum = md5(fl)
+        fl_stat = os.stat(fl)
+        mt = time.ctime(fl_stat.st_mtime)
+        st = str(check_sum) + " " + str(mt)
+        conn.send(st)
+        ans = conn.recv(1024)
+
+def downloadTCP(split_command):
+    filename = split_command[2]
+    hashf = md5(filename)
+    conn.send(hashf)
+    signal = conn.recv(1024)
+    if signal=="yes":
+        with open(filename, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                conn.send(chunk)
+                data = conn.recv(1024)
+                print data
+        conn.send("END")
+        data = conn.recv(1024)
+    else:
+        pass
+
+def downloadUDP(split_command):
+    filename = split_command[2]
+    stat = os.stat(filename)
+    size = str(stat.st_size)
+    mod_t = time.ctime(stat.st_mtime)
+    hashval=md5(filename)
+    conn.send(hashval)
+    signal = conn.recv(1024)
+    if signal=="yes":
+        t_sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        t_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        t_sock.bind(("", 51216))
+        data, (taddr, destinport) = t_sock.recvfrom(1024)
+        print data
+        with open(filename, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                #print(chunk)
+                t_sock.sendto(chunk, (taddr, destinport))
+        t_sock.sendto("END", (taddr, destinport))
+        t_sock.close()
+    else:
+        pass
+
+
+
+
+
 port = 51215
 s = socket.socket()
 host = ""
@@ -23,120 +148,28 @@ while True:
     split_command = command.split()
     if split_command[0]=="index":
         if split_command[1]=="longlist":
-            list_files = os.listdir('.')
-            #s.send('hi')
-            for fl in list_files:
-                info=""
-                fl_stat = os.stat(fl)
-                sz = str(fl_stat.st_size)
-                ct = time.ctime(fl_stat.st_ctime)
-                mt = time.ctime(fl_stat.st_mtime)
-                f_type, code = mimetypes.guess_type(fl, False)
-                if not f_type:
-                    f_type = "Unknown"
-                info+='\t'.join([fl, sz, ct, mt, f_type])+'\n'
-                print info, len(info)
-                conn.send(info)
-                ans = conn.recv(1024)
-                print ans
-            conn.send("123")
+            indexLongList(split_command)
         elif split_command[1]=='shortlist':
-            list_files = os.listdir('.')
-            for fl in list_files:
-                mint=time.mktime(time.strptime(' '.join(split_command[2:6]), '%d %m %Y %H:%M:%S'))
-                maxt=time.mktime(time.strptime(' '.join(split_command[6:10]), '%d %m %Y %H:%M:%S'))
-                info=""
-                fl_stat = os.stat(fl)
-                sz = str(fl_stat.st_size)
-                ct = time.ctime(fl_stat.st_ctime)
-                mt = time.ctime(fl_stat.st_mtime)
-                f_type, code = mimetypes.guess_type(fl, False)
-                if not f_type:
-                    f_type = "Unknown"
-                print mt, mint, maxt
-                if mint <= os.path.getmtime(fl) <=maxt:
-                    info+='\t'.join([fl, sz, ct, mt, f_type])+'\n'
-                    print info, len(info)
-                    conn.send(info)
-                    ans = conn.recv(1024)
-                    print ans
-            conn.send("123")
+            indexShortList(split_command)
         elif split_command[1]=='regex':
-            list_files = os.listdir('.')
-            #s.send('hi')
-            reprog = re.compile(' '.join(split_command[2:]).strip())
-            for fl in list_files:
-                info=""
-                fl_stat = os.stat(fl)
-                sz = str(fl_stat.st_size)
-                ct = time.ctime(fl_stat.st_ctime)
-                mt = time.ctime(fl_stat.st_mtime)
-                f_type, code = mimetypes.guess_type(fl, False)
-                if not f_type:
-                    f_type = "Unknown"
-                if reprog.match(fl):
-                    print fl
-                    info+='\t'.join([fl, sz, ct, mt, f_type])+'\n'
-                    print info, len(info)
-                    conn.send(info)
-                    ans = conn.recv(1024)
-                    print ans
-            conn.send("123")
+            indexRegex(split_command)
     elif split_command[0]=="hash":
         if split_command[1]=="verify":
-            filename = split_command[2]
-            check_sum = md5(filename)
-            fl_stat = os.stat(filename)
-            mt = time.ctime(fl_stat.st_mtime)
-            st = str(check_sum) + " " + str(mt)
-            conn.send(st)
-            ans = conn.recv(1024)
+            hashVerify(split_command)
         elif split_command[1]=="checkall":
-            list_files = os.listdir('.')
-            for fl in list_files:
-                check_sum = md5(fl)
-                fl_stat = os.stat(fl)
-                mt = time.ctime(fl_stat.st_mtime)
-                st = str(check_sum) + " " + str(mt)
-                conn.send(st)
-                ans = conn.recv(1024)
+            hashCheckAll(split_command)
         conn.send("123")
     elif split_command[0]=="download":
         if split_command[1]=="TCP":
-            filename = split_command[2]
-            hashf = md5(filename)
-            with open(filename, "rb") as f:
-                for chunk in iter(lambda: f.read(4096), b""):
-                    conn.send(chunk)
-                    data = conn.recv(1024)
-                    print data
-            conn.send("END")
-            conn.recv(1024)
-            conn.send(hashf)
-            data = conn.recv(1024)
+            downloadTCP(split_command)
         elif split_command[1]=="UDP":
-            filename = split_command[2]
-            t_sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            t_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            t_sock.bind(("", 51216))
-            conn.recv(1024)
-            stat = os.stat(filename)
-            size = str(stat.st_size)
-            mod_t = time.ctime(stat.st_mtime)
-            hashval=md5(filename)
-            fileinfo = str(filename) + ' ' + str(size) + ' ' + str(mod_t) + ' ' + str(hashval) + '\n'
-            conn.send(fileinfo)
-            data, (taddr, destinport) = t_sock.recvfrom(1024)
-            print data
-            with open(filename, "rb") as f:
-                for chunk in iter(lambda: f.read(4096), b""):
-                    #print(chunk)
-                    t_sock.sendto(chunk, (taddr, destinport))
-            t_sock.sendto("END", (taddr, destinport))
-            t_sock.close()
+            downloadUDP(split_command)
         conn.send("123")
         data = conn.recv(1024)
+    elif split_command[0]=="close":
+        print "Closing connection"
+        break
     #print('Server received', repr(data))
-    print('Done sending')
 
 conn.close()
+print('Connection closed')
